51565e331c2efed064fc02e64f74671ede46ea5e
Stadzior <Stadzior@gmail.com>
2016-05-19 17:29:19 +0200

    Injecting executor.

--- src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -5,11 +5,15 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class RadarTest {
 	@Test
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
-		Radar radar = new Radar(batteryMock);
+        Executor executor = Executors.newSingleThreadExecutor();
+        BetterRadar radar = new BetterRadar(batteryMock,executor);
 		radar.notice(new Scud());
 		verify(batteryMock).launchPatriot();
 	}
