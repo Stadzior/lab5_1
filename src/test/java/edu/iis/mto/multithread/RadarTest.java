86c1fa8bf9a7d9723ee3b1b0b506ccd4076d9167
Stadzior <Stadzior@gmail.com>
2016-05-19 18:10:32 +0200

    Final commit.

--- src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,27 +1,28 @@
 package edu.iis.mto.multithread;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class RadarTest {
-	@Test
+
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
+    @Test
+    @Repeat(times = 10)
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Executor executor = new Executor(){
-
-            @Override
-            public void execute(Runnable command) {
-                command.run();
-            }
-        };
+        Executor executor = command -> command.run();
         BetterRadar radar = new BetterRadar(batteryMock,executor);
 		radar.notice(new Scud());
-		verify(batteryMock).launchPatriot();
+		verify(batteryMock,times(10)).launchPatriot();
 	}
 
 }
